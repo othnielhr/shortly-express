efc286620fc6bcdc5421bb178da5c19a8e7a3c1b
Andrew Tran <andrewtran14@gmail.com>
2021-10-27 21:16:04 -0500

    Complete Parsing Cookie

--- server/middleware/cookieParser.js
+++ server/middleware/cookieParser.js
@@ -1,4 +1,18 @@
 const parseCookies = (req, res, next) => {
+  // console.log(req);
+  if (req.headers.cookie) {
+    var cookieStr = req.headers.cookie;
+    // console.log(cookieStr);
+    var cookieArr = cookieStr.split('; ');
+    // console.log(cookieArr);
+    cookieArr.forEach((data) => {
+      var splitCookie = data.split('=');
+      // console.log(splitCookie);
+      req.cookies[splitCookie[0]] = splitCookie[1];
+    });
+  }
+  next();
+  // console.log(req.cookies);
 };
 
 module.exports = parseCookies;
