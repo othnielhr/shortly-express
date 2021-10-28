75c43e43570734d3caed054cca38291556279628
Andrew Tran <andrewtran14@gmail.com>
2021-10-27 22:30:36 -0500

    Working on Createsession Middleware EOD 10.27

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -2,6 +2,21 @@ const models = require('../models');
 const Promise = require('bluebird');
 
 module.exports.createSession = (req, res, next) => {
+  // console.log(req);
+  if (req.headers.cookie) {
+    //has cookie in header
+  } else {
+    models.Sessions.create()
+      .then((data) => {
+        var id = data.insertId;
+        models.Sessions.get({id})
+          .then((sessionData) => {
+            req.session = sessionData;
+            res.cookie('shortlyid', req.session.hash);
+            next();
+          });
+      });
+  }
 };
 
 /************************************************************/
